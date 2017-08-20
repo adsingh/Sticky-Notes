134f7ddba96a776ffb2fa22163c5674c68c499e8
adsingh <ads7194@gmail.com>
2017-08-20 00:02:03 -0400

    Live pages

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import Board from './Board'
-
-var app = document.getElementById('react-container');
-
-ReactDOM.render(<Board />, app)
--- src/js/index_final.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Board from './Board'
-
-var app = document.getElementById('react-container');
-
-ReactDOM.render(<Board count={10} />, app);
